2b13f7602c0a85508a1fb510c8f88f3630f0a6de
Dirk380 <dirkvmunster@gmail.com>
2023-02-13 16:04:32 +0100

    Started with dogRepo test

--- DogApplication-master/src/test/java/com/example/dogapplication/Repository/DogRepositoryTest.java
+++ DogApplication-master/src/test/java/com/example/dogapplication/Repository/DogRepositoryTest.java
@@ -4,11 +4,14 @@ import com.example.dogapplication.Model.Dog;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 class DogRepositoryTest {
 
     @Autowired
     private DogRepository dogRepositoryTest;
+
 
     @Test
     void existsDogByDogId() {
@@ -19,7 +22,11 @@ class DogRepositoryTest {
         //given
         Dog dog = new Dog(1L, "Silas", 13 , "Malinous", "Male","Brown");
         //when
+        dogRepositoryTest.save(dog);
 
+        boolean exist = dogRepositoryTest.checkDogName(dog.getDogName());
         //then
+        assertThat(exist).isTrue();
+
     }
 }
